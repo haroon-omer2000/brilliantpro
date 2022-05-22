a6dec52e2e86be58418660a20e7ed93c4760bb13
Haroon Omer <haroonomer@Haroons-MacBook-Air.local>
2022-05-22 18:21:42 +0500

    setup temporary backend for react

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+var cors = require('cors');
+app.use(cors());
+var http = require('http'); 
+
+let bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+  
+var mongoDB = 'mongodb://localhost:27017/web-class';
+
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}) );
+
+app.get('/test', function (req, res) {
+    console.log('Recieved')
+    res.send({response:"successx"});
+});
+
+app.listen(4000,()=>{
+    console.log('Listening on port 4000');
+});
+
--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
+  useEffect(() =>{
+    
+  },[])
+
+  const test = async () => {
+    fetch('http://localhost:4000/test',{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    }).then((response) => response.json()).then( data => {
+         console.log(data); 
+      })
+  }
+
   return (
     <div>
       <h1>Hello world</h1>
+      <button onClick={test}>Fetch</button>
     </div>
   );
 }
