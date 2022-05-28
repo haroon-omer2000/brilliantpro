cea255c91eddeace5bd688efaf099567cc7ce444
Haroon Omer <haroonomer@Haroons-MacBook-Air.local>
2022-05-28 15:37:13 +0500

    update show page to contain links to course materials and assessments

--- src/components/DisplayCourse.js
+++ src/components/DisplayCourse.js
@@ -21,17 +21,70 @@ const DisplayCourse = () => {
   },[])
 
   return (
-    <div className="card h-100 text-center">
-     <img src={url} className="courses-img card-img-top" alt='...'/>
-        <div className="card-body">
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">{overview}</p>
-          <p className="card-text"><strong>Completion time: </strong>{weeks} weeks</p>
+    <div class="accordion" id="accordionPanelsStayOpenExample">
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+            {title}
+          </button>
+        </h2>
+        <img src={url} class="rounded flex-start" alt="..." />
+        <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+          <div class="accordion-body">
+            <div className='course-content'>
+              <strong className='course-show-heading'>Overview</strong>{overview}
+              <p><strong className='course-show-heading'>Weeks to complete: </strong>{weeks}</p>
+            </div>
+          </div>
         </div>
-        <div className='card-option'>
-          <button className="btn btn-primary" type="submit">Details</button>
-        </div> 
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+            Course Assesments
+          </button>
+        </h2>
+        <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+          <div class="accordion-body">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Quizzes</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Assignments</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+            Course Materials
+          </button>
+        </h2>
+        <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+          <div class="accordion-body">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="#">View Materials</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
+    // <div className="card h-100 text-center">
+    //  <img src={url} className="courses-img card-img-top" alt='...'/>
+    //     <div className="card-body">
+    //       <h5 className="card-title">{title}</h5>
+    //       <p className="card-text">{overview}</p>
+    //       <p className="card-text"><strong>Completion time: </strong>{weeks} weeks</p>
+    //     </div>
+    //     <div className='card-option'>
+    //       <button className="btn btn-primary" type="submit">Details</button>
+    //     </div> 
+    // </div>
   )
 }
 
